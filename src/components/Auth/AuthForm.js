d18d0445124c9e3f066bd76d079701740af7dded
Brian Duong <brianminhduong@gmail.com>
2022-08-11 20:31:53 -0400

    Redirect user to main page after login

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,4 +1,5 @@
 import { useState, useRef, useContext } from "react";
+import { useHistory } from "react-router-dom";
 
 import classes from "./AuthForm.module.css";
 import AuthContext from "../../store/auth-context";
@@ -6,6 +7,7 @@ import AuthContext from "../../store/auth-context";
 const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+  const history = useHistory();
 
   const authCtx = useContext(AuthContext);
 
@@ -60,6 +62,7 @@ const AuthForm = () => {
       })
       .then((data) => {
         authCtx.login(data.idToken);
+        history.replace("/");
       })
       .catch((err) => {
         alert(err.message);
